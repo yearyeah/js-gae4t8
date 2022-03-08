bc8dd60ade316d5c4fd0aa5b096c33f8f4213061
yearyeah <340222220@qq.com>
2022-03-08 09:24:34 +0800

    1 file modified, 2 added

--- snippet/JSTPost API DEMO.js
+++ snippet/JSTPost API DEMO.js
@@ -20,50 +20,20 @@ JSTPost({
   contentType: 'application/json;charset=utf-8',
 });
 
-
-
-var freeToScm = {
-  config: {
-    api: '/erp/webapi/UserApi/Company/GetShopTypes',
+JSTPost({
+  url: '/erp/webapi/JGD/JGDPlatformInformationOperate/IsPay',
+  isNeedBaseUrl: true,
+  data: JSON.stringify({
+    coid: $.cookie('u_co_id') || '0',
+    uid: $.cookie('u_id') || '0',
+    data: {},
+  }),
+  success: function (res) {
+    console.log('success', res);
   },
-  init: function () {
-    var basePopUrl = window.top.isProd
-      ? window.top.location.hostname.indexOf('daili') > -1
-        ? 'https://api.jushuitan.com'
-        : 'https://api.erp321.com'
-      : 'https://dev-api.jushuitan.com';
-
-    //testApiUrl = 'http://mydev.jst.com/webapi/UserApi/Company/GetShopTypes';
-
-    var requestData = {
-      coid: '0',
-      uid: '0',
-      data: {
-        coid: '0',
-      },
-    };
-
-    try {
-      requestData.coid = $.cookie('u_co_id');
-      requestData.data.coid = $.cookie('u_co_id');
-      requestData.uid = $.cookie('u_id');
-    } catch (e) {
-      window.console && console.error('Error:' + e);
-    }
-
-    JSTPost({
-      url: basePopUrl + this.config.api,
-      //url: testApiUrl,
-      data: JSON.stringify(requestData),
-      success: function (res) {
-        console.log('success', res);
-        var platformList = res.data.map((item) => {
-          return item.shopType;
-        });
-        console.log('platformList', platformList);
-      },
-      contentType: 'application/json;charset=utf-8',
-    });
+  error: function (err) {
+    console.log(err);
+    _this._init(options);
   },
-};
-freeToScm.init();
+  contentType: 'application/json;charset=utf-8',
+});
